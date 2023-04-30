832fc714e34b6fcac8294773ac31bd27f40ed2ab
Nayalash <1nayalashm@gmail.com>
2023-04-30 13:24:32 -0400

    fix cors

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"GoProject/external"
 	"GoProject/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"log"
 )
 
@@ -36,6 +37,13 @@ func setupRoutes(app *fiber.App) {
 func main() {
 	database.ConnectDb()
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
 
 	setupRoutes(app)
 
